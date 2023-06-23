2af4b5babcf8ddfdb60cdc65458db385b59ebd72
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2023-06-24 02:53:13 +0900

    feat: register controllers

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,20 @@ import (
 	"os"
 	"regexp"
 
+	firebase "firebase.google.com/go"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/shunsukenagashima/chat-api/pkg/domain/model"
+	"github.com/shunsukenagashima/chat-api/pkg/infra/auth"
 	"github.com/shunsukenagashima/chat-api/pkg/infra/repository"
 	"github.com/shunsukenagashima/chat-api/pkg/interface/controller"
 	"github.com/shunsukenagashima/chat-api/pkg/interface/route"
 	"github.com/shunsukenagashima/chat-api/pkg/usecase"
+	"google.golang.org/api/option"
 )
 
 func main() {
@@ -28,35 +32,67 @@ func main() {
 func run(ctx context.Context) error {
 	router := gin.Default()
 
-	controllers, err := initializeControllers()
+	controllers, err := initializeControllers(ctx)
 	if err != nil {
 		return err
 	}
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+
+	router.Use(cors.New(corsConfig))
 
 	route.RegisterRoutes(router, controllers)
 
 	return router.Run()
 }
 
-func initializeControllers() (*controller.Controllers, error) {
+func initializeControllers(ctx context.Context) (*controller.Controllers, error) {
 	hm := model.NewHubManager()
 
-	sess, _ := session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 		Region:   aws.String("us-west-2"),
-		Endpoint: aws.String("http://localhost:8000"),
+		Endpoint: aws.String("http://dynamodb-local:8000"),
 	})
+	if err != nil {
+		return nil, err
+	}
 	db := dynamodb.New(sess)
 
+	credentials, err := os.ReadFile("/run/secrets/firebase-credentials")
+	if err != nil {
+		return nil, err
+	}
+	opt := option.WithCredentialsJSON(credentials)
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		return nil, err
+	}
+	client, err := app.Auth(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fa := auth.NewFirebaseAuth(client)
+
 	rr := repository.NewRoomRepository(db)
-	ru := usecase.NewRoomUsecase(rr)
+	rur := repository.NewRoomUserRepository(db)
+	ur := repository.NewUserRepository(db)
+
+	ru := usecase.NewRoomUsecase(rr, ur)
+	ruu := usecase.NewRoomUserUsecase(rur, ur, rr)
+	uu := usecase.NewUserUsecase(ur, fa)
+
 	v := validator.New()
 
 	v.RegisterValidation("alnumdash", isAlnumOrDash)
 
 	controllers := &controller.Controllers{
-		HelloController: controller.NewHelloController(),
-		WSController:    controller.NewWSController(hm),
-		RoomController:  controller.NewRoomController(ru, v),
+		HelloController:    controller.NewHelloController(),
+		WSController:       controller.NewWSController(hm),
+		RoomController:     controller.NewRoomController(ru, v),
+		RoomUserController: controller.NewRoomUserController(ruu, v),
+		UserController:     controller.NewUserController(uu, v),
 	}
 
 	return controllers, nil
