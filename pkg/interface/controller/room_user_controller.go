2ccadb8612447803ed44a32c046d8df70eea653e
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2023-07-01 00:05:28 +0900

    fix: return key

--- pkg/interface/controller/room_user_controller.go
+++ pkg/interface/controller/room_user_controller.go
@@ -37,11 +37,11 @@ func (rc *RoomUserController) RemoveUserFromRoom(ctx *gin.Context) {
 	userId := ctx.Param("userId")
 
 	if err := rc.roomUserUsecase.RemoveUserFromRoom(ctx, roomId, userId); err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "success to remove the user from the room"})
+	ctx.JSON(http.StatusOK, gin.H{"result": "success to remove the user from the room"})
 }
 
 func (rc *RoomUserController) AddUsersToRoom(ctx *gin.Context) {
@@ -51,14 +51,14 @@ func (rc *RoomUserController) AddUsersToRoom(ctx *gin.Context) {
 	}
 
 	if err := ctx.BindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	if err := rc.roomUserUsecase.AddUsersToRoom(ctx, req.RoomID, req.UserIDs); err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, gin.H{"message": "success to add the users to the room"})
+	ctx.JSON(http.StatusCreated, gin.H{"result": "success to add the users to the room"})
 }
